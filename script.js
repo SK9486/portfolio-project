5a738f9ec1204ee2fc04fd4668cbbd925c54908c
SK9486 <valli74appan@gmail.com>
2026-06-16 23:44:22 +0530

    menu bar ready

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.getElementById("menu-btn");
 const sidebar = document.getElementById("sidebar");
-
+const closeBtn = document.getElementById("close-btn");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+});
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
 });
