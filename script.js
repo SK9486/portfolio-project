764bebcc43a636fd33513a56852acad0ecfe1ebe
SK9486 <valli74appan@gmail.com>
2026-06-19 02:14:46 +0530

    tablet version of app is ready

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menu-btn");
 const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("close-btn");
+const navbar = document.querySelector(".links");
+const gotoTop = document.getElementById("up-icon");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   menuBtn.classList.add("hidden");
@@ -8,4 +10,17 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
   menuBtn.classList.remove("hidden");
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+gotoTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
