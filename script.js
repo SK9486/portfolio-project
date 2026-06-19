c669662128fa16aa07211857f98cb7e318180514
SK9486 <valli74appan@gmail.com>
2026-06-19 13:46:08 +0530

    desktop size responsive ready

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("close-btn");
 const navbar = document.querySelector(".links");
 const gotoTop = document.getElementById("up-icon");
+const cvButn = document.getElementById("cv-but");
+
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   menuBtn.classList.add("hidden");
@@ -23,4 +25,8 @@ gotoTop.addEventListener("click", () => {
     top: 0,
     behavior: "smooth",
   });
+});
+cvButn.addEventListener("click", () => {
+  window.location.href =
+    "https://drive.google.com/file/d/1qqgKuU6F4EZ4xWQis0kd-672Y2KBcy43/view?usp=sharing";
 });
